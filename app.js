4bb690edc5405054d956b14b3f62d67f9c3d27b4
Leonardo Jose  Schettini de Arruda <a1601323@unet.univie.ac.at>
2017-01-17 20:55:52 +0100

    Implement web service communication methods

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var request = require('request');
 var fs = require('fs');
 var busboy = require('express-busboy');
 var js2xmlparser = require("js2xmlparser");
+var xml2jsparser = require('xml2js').Parser();
 
 var app = express();
 busboy.extend(app, {
@@ -30,8 +31,6 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
-var GlobalResponse;
-
 app.post('/postVote', function(req, res){
 
 	// var options = {
@@ -52,7 +51,6 @@ app.post('/postVote', function(req, res){
 						console.log('received ' + data.length + ' bytes of compressed data')
 						console.log(data)
 					})
-					GlobalResponse = response;
 				})
 				.on('data', function(data) {
 					// decompressed data as it is received 
@@ -98,17 +96,17 @@ app.post('/sendXml', function(req, res){
 app.post('/upload', function(req, res) {
 	
 	// Get file from HTML
-	var loader = req.files.loader;
+	var fileloader = req.files.fileloader;
 
-	fs.createReadStream(loader.file).on('data', function(dataBuffer) {
+	fs.createReadStream(fileloader.file).on('data', function(dataBuffer) {
 		// Parse file to xml
 		var xml = js2xmlparser.parse("Element", {
-			"key": loader.filename, // Get file name (including extension)
+			"key": fileloader.filename, // Get file name (including extension)
 			"data": JSON.stringify(dataBuffer.toJSON().data), // Read buffer from range
 		})
 
 		var json = JSON.stringify({
-			"key": loader.filename, // Get file name (including extension)
+			"key": fileloader.filename, // Get file name (including extension)
 			"data": dataBuffer.toJSON().data,
 		})
 
@@ -116,7 +114,7 @@ app.post('/upload', function(req, res) {
 		// console.log(json);
 
 		// test with string
-		// fs.writeFile(loader.filename, dataBuffer, (err) => {
+		// fs.writeFile(fileloader.filename, dataBuffer, (err) => {
 		// 	if (err) throw err;
 		// 	console.log('It\'s saved!');
 		// });
@@ -125,6 +123,123 @@ app.post('/upload', function(req, res) {
 		var options = {
 			method: 'POST',
 			uri: 'http://localhost:8080/CloudStorage/storeFile',
+			body: xml,
+			headers: {'Content-Type': 'text/xml'},
+		};
+		
+		// Send request to web service
+		request(options, function (error, response, body) {
+			console.log('REQUEST DONE');
+			if (!error && response.statusCode == 200) {
+				// TODO: catch errors and set proper message to frontend (redirect/render properly)
+				res.redirect('/');
+			}
+		});
+	});
+
+});
+
+app.post('/getFile', function(req, res) {
+	console.log('AEE ' + req.body.filename);
+	request
+		.get('http://localhost:8080/CloudStorage/getFile/?key=' + req.body.filename)
+		.on('response', function(response) {
+			
+			console.log('RESPONSE');
+			console.log(response.statusCode); // 200 
+			if (response.statusCode == 200) {
+				// TODO: catch errors and set proper message to frontend (redirect/render properly)
+				res.redirect('/');
+			}
+
+		})
+		.on('data', function(data) {
+			xml2jsparser.parseString(data, function (err, data) {
+				if(!err)
+				{
+					console.dir(data);
+					fs.writeFile('/downloads/' + req.body.filename, data, (err) => {
+						if (err) throw err;
+						console.log('It\'s saved!');
+					});
+
+				} else{
+					console.log(err);
+				}
+			});
+			
+			
+		});
+});
+
+app.post('/deleteFile', function(req, res) {
+//onsubmit="return updateLoaderAction()"
+	console.log('AEE ' + req.body.filename);
+	request
+		.get('http://localhost:8080/CloudStorage/delete/?key=' + req.body.filename)
+		.on('response', function(response) {
+			
+			console.log('RESPONSE');
+			console.log(response.statusCode); // 200 
+			if (response.statusCode == 200) {
+				// TODO: catch errors and set proper message to frontend (redirect/render properly)
+				res.redirect('/');
+			}
+
+		})
+});
+
+app.post('/exportDatabase', function(req, res) {
+
+	request
+		.get('http://localhost:8080/CloudStorage/exportDatabase')
+		.on('response', function(response) {
+			
+			console.log('RESPONSE');
+			console.log(response.statusCode); // 200 
+			if (response.statusCode == 200) {
+				// TODO: catch errors and set proper message to frontend (redirect/render properly)
+				res.redirect('/');
+			}
+
+		})
+		.on('data', function(data) {
+			xml2jsparser.parseString(data, function (err, data) {
+				if(!err)
+				{
+					var today = new Date();
+
+					console.dir(data);
+					fs.writeFile('/downloads/database_backup' + today.now(), data, (err) => {
+						if (err) throw err;
+						console.log('It\'s saved!');
+					});
+
+				} else{
+					console.log(err);
+				}
+			});
+			
+			
+		});
+});
+
+app.post('/importDatabase', function(req, res) {
+	
+	// Get file from HTML
+	var fileloader = req.files.fileloader;
+
+	fs.createReadStream(fileloader.file).on('data', function(dataBuffer) {
+		// Parse file to xml
+		var xml = js2xmlparser.parse("Element", {
+			"key": fileloader.filename, // Get file name (including extension)
+			"data": JSON.stringify(dataBuffer.toJSON().data), // Read buffer from range
+		})
+
+		// Set request options
+		var options = {
+			method: 'POST',
+			uri: 'http://localhost:8080/CloudStorage/importDatabase',
 			body: xml,
 			headers: {'Content-Type': 'text/xml'},
 		};
